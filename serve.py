bdcecdc9c215a1c62288f7f62ffd21b1b191061c
dakshg3 <157659105+dakshg3@users.noreply.github.com>
2024-01-29 19:31:29 -0600

    Add files via upload

---
+++ serve.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import subprocess
+import socket
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def handle_request():
+    if request.method == 'POST':
+        subprocess.Popen(["python3", "stress_cpu.py"])
+        #return "Started stress_cpu.py"
+
+    elif request.method == 'GET':
+        private_ip = socket.gethostbyname(socket.gethostname())
+        return f"{private_ip}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
